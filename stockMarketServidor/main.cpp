5cd6d95e7ad742ca52cf7fd8208d677bddddf9fb
Pablo Díez <pablo.diez@opendeusto.es>
2025-05-17 10:42:09 +0200

    commit

--- stockMarketServidor/main.cpp
+++ stockMarketServidor/main.cpp
@@ -1,13 +1,6 @@
-/*
- * main.c
- *
- *  Created on: 16 abr 2025
- *      Author: pablo.diez
- */
-
-
 #include <iostream>
 #include <winsock2.h>
+#include <string>
 
 #define SERVER_IP "127.0.0.1"
 #define SERVER_PORT 6000
@@ -16,117 +9,113 @@ int main() {
     WSADATA wsaData;
     SOCKET conn_socket, comm_socket;
     sockaddr_in server{}, client{};
-    char sendBuff[512], recvBuff[512];
+    char recvBuff[512];
 
     std::cout << "\nInitialising Winsock...\n";
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        std::cerr << "Failed. Error Code: " << WSAGetLastError() << '\n';
+        std::cerr << "WSAStartup failed. Error: " << WSAGetLastError() << '\n';
         return -1;
     }
 
-    std::cout << "Initialised.\n";
-
-    // Crear socket
     conn_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (conn_socket == INVALID_SOCKET) {
-        std::cerr << "Could not create socket: " << WSAGetLastError() << '\n';
+        std::cerr << "Socket creation failed. Error: " << WSAGetLastError() << '\n';
         WSACleanup();
         return -1;
     }
-
-    std::cout << "Socket created.\n";
 
     server.sin_addr.s_addr = inet_addr(SERVER_IP);
     server.sin_family = AF_INET;
     server.sin_port = htons(SERVER_PORT);
 
-    // Asociar IP/puerto al socket
     if (bind(conn_socket, (sockaddr*)&server, sizeof(server)) == SOCKET_ERROR) {
-        std::cerr << "Bind failed with error code: " << WSAGetLastError() << '\n';
+        std::cerr << "Bind failed. Error: " << WSAGetLastError() << '\n';
         closesocket(conn_socket);
         WSACleanup();
         return -1;
     }
 
-    std::cout << "Bind done.\n";
-
-    // Escuchar conexiones entrantes
     if (listen(conn_socket, 1) == SOCKET_ERROR) {
-        std::cerr << "Listen failed with error code: " << WSAGetLastError() << '\n';
+        std::cerr << "Listen failed. Error: " << WSAGetLastError() << '\n';
         closesocket(conn_socket);
         WSACleanup();
         return -1;
     }
 
-    std::cout << "Waiting for incoming connections...\n";
-    int stsize = sizeof(sockaddr);
-    comm_socket = accept(conn_socket, (sockaddr*)&client, &stsize);
+    std::cout << "Waiting for incoming connection...\n";
+    int client_size = sizeof(sockaddr);
+    comm_socket = accept(conn_socket, (sockaddr*)&client, &client_size);
     if (comm_socket == INVALID_SOCKET) {
-        std::cerr << "Accept failed with error code: " << WSAGetLastError() << '\n';
+        std::cerr << "Accept failed. Error: " << WSAGetLastError() << '\n';
         closesocket(conn_socket);
         WSACleanup();
         return -1;
     }
 
-    std::cout << "Incoming connection from: " << inet_ntoa(client.sin_addr)
-              << " (" << ntohs(client.sin_port) << ")\n";
+    std::cout << "Client connected: " << inet_ntoa(client.sin_addr)
+              << ":" << ntohs(client.sin_port) << "\n";
 
-    // Cerrar el socket de escucha
     closesocket(conn_socket);
 
-    std::cout << "Waiting for incoming messages from client...\n";
+    // Enviar menú
+    std::string menu = "===== MENU =====\n1. Iniciar sesion\n2. Registrarse\n3. Salir\nSeleccione una opcion: ";
+    send(comm_socket, menu.c_str(), menu.size(), 0);
 
-    while (true) {
-        int bytes = recv(comm_socket, recvBuff, sizeof(recvBuff), 0);
-        if (bytes <= 0) break;
-
-        recvBuff[bytes] = '\0';
-        std::string option(recvBuff);
-
-        if (option == "1") {
-            std::cout << "Client chose to login.\n";
-
-            send(comm_socket, "Enter username:", 16, 0);
-            bytes = recv(comm_socket, recvBuff, sizeof(recvBuff), 0);
-            recvBuff[bytes] = '\0';
-            std::string username(recvBuff);
-
-            send(comm_socket, "Enter password:", 16, 0);
-            bytes = recv(comm_socket, recvBuff, sizeof(recvBuff), 0);
-            recvBuff[bytes] = '\0';
-            std::string password(recvBuff);
-
-            // Aquí deberías validar las credenciales (ej. desde archivo)
-            std::cout << "Received login: " << username << " / " << password << "\n";
-            send(comm_socket, "Login received.\n", 17, 0);
-
-        } else if (option == "2") {
-            std::cout << "Client chose to register.\n";
-
-            send(comm_socket, "Choose a username:", 19, 0);
-            bytes = recv(comm_socket, recvBuff, sizeof(recvBuff), 0);
-            recvBuff[bytes] = '\0';
-            std::string newUser(recvBuff);
-
-            send(comm_socket, "Choose a password:", 19, 0);
-            bytes = recv(comm_socket, recvBuff, sizeof(recvBuff), 0);
-            recvBuff[bytes] = '\0';
-            std::string newPass(recvBuff);
-
-            // Aquí podrías guardar el usuario en un archivo o base de datos
-            std::cout << "New user registered: " << newUser << " / " << newPass << "\n";
-            send(comm_socket, "Registration completed.\n", 25, 0);
-
-        } else if (option == "3") {
-            std::cout << "Client chose to exit.\n";
-            send(comm_socket, "Goodbye!\n", 9, 0);
-            break;
-
-        } else {
-            send(comm_socket, "Invalid option.\n", 16, 0);
-        }
+    // Recibir opción
+    int bytes = recv(comm_socket, recvBuff, sizeof(recvBuff) - 1, 0);
+    if (bytes <= 0) {
+        std::cerr << "Client disconnected unexpectedly.\n";
+        closesocket(comm_socket);
+        WSACleanup();
+        return 0;
     }
 
+    recvBuff[bytes] = '\0';
+    std::string option(recvBuff);
+
+    if (option == "1") {
+        std::cout << "Client selected login.\n";
+
+        send(comm_socket, "Enter username: ", 17, 0);
+        bytes = recv(comm_socket, recvBuff, sizeof(recvBuff) - 1, 0);
+        recvBuff[bytes] = '\0';
+        std::string username(recvBuff);
+
+        send(comm_socket, "Enter password: ", 17, 0);
+        bytes = recv(comm_socket, recvBuff, sizeof(recvBuff) - 1, 0);
+        recvBuff[bytes] = '\0';
+        std::string password(recvBuff);
+
+        std::cout << "Login attempt: " << username << " / " << password << "\n";
+
+        // Aquí puedes validar con la base de datos
+        send(comm_socket, "Login recibido.\n", 17, 0);
+
+    } else if (option == "2") {
+        std::cout << "Client selected register.\n";
+
+        send(comm_socket, "Choose username: ", 18, 0);
+        bytes = recv(comm_socket, recvBuff, sizeof(recvBuff) - 1, 0);
+        recvBuff[bytes] = '\0';
+        std::string newUser(recvBuff);
+
+        send(comm_socket, "Choose password: ", 18, 0);
+        bytes = recv(comm_socket, recvBuff, sizeof(recvBuff) - 1, 0);
+        recvBuff[bytes] = '\0';
+        std::string newPass(recvBuff);
+
+        std::cout << "New user: " << newUser << " / " << newPass << "\n";
+
+        // Aquí puedes registrar el usuario en la base de datos
+        send(comm_socket, "Registro completado.\n", 23, 0);
+
+    } else if (option == "3") {
+        std::cout << "Client chose to exit.\n";
+        send(comm_socket, "Adiós.\n", 7, 0);
+    } else {
+        std::cout << "Opción inválida recibida.\n";
+        send(comm_socket, "Opción inválida.\n", 17, 0);
+    }
 
     closesocket(comm_socket);
     WSACleanup();
